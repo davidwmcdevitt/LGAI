809026527bab4274366784782a6dc4c67930840c
davidwmcdevitt <31931152+davidwmcdevitt@users.noreply.github.com>
2023-10-26 17:04:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,5 +74,8 @@ if st.session_state.messages[-1]["role"] != "assistant":
                 full_response += item
                 placeholder.markdown(full_response)
             placeholder.markdown(full_response)
+
+    full_response = [full_response, type(full_response)]
     message = {"role": "assistant", "content": full_response}
+    
     st.session_state.messages.append(message)
