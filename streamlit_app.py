f5b6fe2cb8db531b1401dc58893070d341c8a4b1
davidwmcdevitt <31931152+davidwmcdevitt@users.noreply.github.com>
2023-10-26 17:53:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ with st.sidebar:
     temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
     top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
     max_length = st.sidebar.slider('max_length', min_value=32, max_value=128, value=120, step=8)
-    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
+    #st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
@@ -42,6 +42,12 @@ for message in st.session_state.messages:
         if message["role"] == "assistant":
         
             st.write("-"*50)
+            
+            if copyright_eval == "No Risk":
+                copyright_msg = "\n\n No copyright infringement has been detected in these outputs."
+    
+
+            st.write(copyright_msg)
         
         #st.write(copyright_msg)
 
